a437ded7bba9ac3a41d6a5969b2e6d65330ac214
Koy Saeteurn <kosatun@aol.com>
2018-07-25 20:17:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,12 @@ end
 
 
 def batch_badge_creator(array)
-  array.each {|name| puts 
-  
+  array.each {|name| puts "Hello, my name is #{name}."}
+end 
+
+def assign_rooms(array)
+  rooms = 1 
+  array.each do |speaker|
+    puts "Hello, #{speaker}! You'll be assigned to room #{rooms}!"
+    rooms += 1
 end  
