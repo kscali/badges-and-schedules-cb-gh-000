e00c7383df535c87031f63a31bd24b500714c34f
Koy Saeteurn <kosatun@aol.com>
2018-07-25 21:06:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,7 +24,7 @@ def assign_rooms(array)
 end  
 
 def printer(attendee)
-  puts batch_badge_creator(array)
-  puts assign_rooms(array)
+  puts batch_badge_creator(attendee)
+  puts assign_rooms(attendee)
 end  
   
